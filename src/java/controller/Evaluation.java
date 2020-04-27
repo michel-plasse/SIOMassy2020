5f3499fcdac6c9bf871e8827dec4c5b88e1371b2
reda Zeryouhi <reda.zeryouhi@gmail.com>
2020-04-27 18:57:53 +0200

    commit 21 : création de la classe Evaluation

---
+++ src/java/controller/Evaluation.java
@@ -0,0 +1,72 @@
+package controller;
+
+import java.time.LocalDateTime;
+
+public class Evaluation {
+    private int idEvaluation;
+    private LocalDateTime dateEffet;
+    private int idCreateur;
+    private int idSessionFormation;
+    private int idModule;
+
+    
+    // ##################################    Constructeurs    ############################################
+    
+    public Evaluation() {
+    }
+
+    public Evaluation(int idEvaluation, LocalDateTime dateEffet, int idCreateur, int idSessionFormation, int idModule) {
+        this.idEvaluation = idEvaluation;
+        this.dateEffet = dateEffet;
+        this.idCreateur = idCreateur;
+        this.idSessionFormation = idSessionFormation;
+        this.idModule = idModule;
+    }
+
+    
+    
+    // ############################### Getters et Setters ##################################################
+    public int getIdEvaluation() {
+        return idEvaluation;
+    }
+
+    public void setIdEvaluation(int idEvaluation) {
+        this.idEvaluation = idEvaluation;
+    }
+
+    public LocalDateTime getDateEffet() {
+        return dateEffet;
+    }
+
+    public void setDateEffet(LocalDateTime dateEffet) {
+        this.dateEffet = dateEffet;
+    }
+
+    public int getIdCreateur() {
+        return idCreateur;
+    }
+
+    public void setIdCreateur(int idCreateur) {
+        this.idCreateur = idCreateur;
+    }
+
+    public int getIdSessionFormation() {
+        return idSessionFormation;
+    }
+
+    public void setIdSessionFormation(int idSessionFormation) {
+        this.idSessionFormation = idSessionFormation;
+    }
+
+    public int getIdModule() {
+        return idModule;
+    }
+
+    public void setIdModule(int idModule) {
+        this.idModule = idModule;
+    }
+    
+    
+    
+    
+}
