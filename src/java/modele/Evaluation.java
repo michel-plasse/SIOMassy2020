1084aa9fe2defd88b4f7289b8005b97a847f279a
kenzy <kenzy@kenzybnt@hotmail.com>
2020-05-13 17:27:31 +0200

    creation de la classe Evaluation.Java dans modele

---
+++ src/java/modele/Evaluation.java
@@ -0,0 +1,13 @@
+package modele;
+
+import java.time.LocalDateTime;
+
+public class Evaluation {
+
+    private int idEvaluation;
+    private LocalDateTime dateEffet;
+    private int idCreateur;
+    private int idSessionFormation;
+    private int idModule;
+    
+}
