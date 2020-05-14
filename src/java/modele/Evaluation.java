b8b591465bf9cb1a039ade7a77a03dad624a15ad
kenzy <kenzy@kenzybnt@hotmail.com>
2020-05-14 17:42:55 +0200

    modification Evaluation.Java dans modele

--- src/java/modele/Evaluation.java
+++ src/java/modele/Evaluation.java
@@ -9,5 +9,58 @@ public class Evaluation {
     private int idCreateur;
     private int idSessionFormation;
     private int idModule;
-    
+
+    // ##################################    Constructeurs    ############################################
+    public Evaluation() {
+    }
+
+    public Evaluation(int idEvaluation, LocalDateTime dateEffet, int idCreateur, int idSessionFormation, int idModule) {
+        this.idEvaluation = idEvaluation;
+        this.dateEffet = dateEffet;
+        this.idCreateur = idCreateur;
+        this.idSessionFormation = idSessionFormation;
+        this.idModule = idModule;
+    }
+
+    // ############################### Getters et Setters ##################################################
+    public int getIdEvaluation() {
+        return idEvaluation;
+    }
+
+    public void setIdEvaluation(int idEvaluation) {
+        this.idEvaluation = idEvaluation;
+    }
+
+    public LocalDateTime getDateEffet() {
+        return dateEffet;
+    }
+
+    public void setDateEffet(LocalDateTime dateEffet) {
+        this.dateEffet = dateEffet;
+    }
+
+    public int getIdCreateur() {
+        return idCreateur;
+    }
+
+    public void setIdCreateur(int idCreateur) {
+        this.idCreateur = idCreateur;
+    }
+
+    public int getIdSessionFormation() {
+        return idSessionFormation;
+    }
+
+    public void setIdSessionFormation(int idSessionFormation) {
+        this.idSessionFormation = idSessionFormation;
+    }
+
+    public int getIdModule() {
+        return idModule;
+    }
+
+    public void setIdModule(int idModule) {
+        this.idModule = idModule;
+    }
+
 }
