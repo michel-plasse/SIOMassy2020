44990830faa8a60bdec37c7d1fc5065adc2dc3b3
reda Zeryouhi <reda.zeryouhi@gmail.com>
2020-05-15 00:26:28 +0200

    Ajout EvaluationDao + fix Trombi

---
+++ src/java/dao/EvaluationDao.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import modele.Evaluation;
+import modele.Personne;
+
+/**
+ *
+ * @author azery
+ */
+public class EvaluationDao {
+  
+  public static List<Personne> getEvaluationByFormateur() throws SQLException {
+        Connection con = Database.getConnection();
+        List<Personne> result = new ArrayList();
+        String requete = "SELECT * FROM personne";
+        PreparedStatement canal = con.prepareStatement(requete);
+        //canal.setInt(1, idFormateur);
+        ResultSet rs = canal.executeQuery();
+        while (rs.next()) {
+            /*Evaluation evaluation = new Evaluation(
+                    rs.getInt("id_evaluation"), 
+                    rs.getTimestamp("date_effet").toLocalDateTime(),                     
+                    rs.getInt("id_createur"), 
+                    rs.getInt("id_session_formation"),
+                    rs.getInt("id_module"));
+                    
+            result.add(evaluation);*/
+            Personne personne = new Personne(
+              rs.getInt("id_personne"),
+              rs.getString("prenom"),
+              rs.getString("nom"),
+              rs.getString("email"));
+            
+            result.add(personne);
+        }
+        return result;
+    }
+  
+}
