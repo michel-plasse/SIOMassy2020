8d1421055090652bc48e5fb55bdd77c7d7192fc4
reda Zeryouhi <reda.zeryouhi@gmail.com>
2020-05-18 01:54:09 +0200

    fix methode getEvaluationByFormateur(int idFormateur) : test OK

--- src/java/dao/EvaluationDao.java
+++ src/java/dao/EvaluationDao.java
@@ -12,39 +12,35 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import modele.Evaluation;
-import modele.Personne;
 
 /**
  *
  * @author azery
  */
 public class EvaluationDao {
-  
-  public static List<Personne> getEvaluationByFormateur() throws SQLException {
-        Connection con = Database.getConnection();
-        List<Personne> result = new ArrayList();
-        String requete = "SELECT * FROM personne";
-        PreparedStatement canal = con.prepareStatement(requete);
-        //canal.setInt(1, idFormateur);
-        ResultSet rs = canal.executeQuery();
-        while (rs.next()) {
-            /*Evaluation evaluation = new Evaluation(
-                    rs.getInt("id_evaluation"), 
-                    rs.getTimestamp("date_effet").toLocalDateTime(),                     
-                    rs.getInt("id_createur"), 
-                    rs.getInt("id_session_formation"),
-                    rs.getInt("id_module"));
-                    
-            result.add(evaluation);*/
-            Personne personne = new Personne(
-              rs.getInt("id_personne"),
-              rs.getString("prenom"),
-              rs.getString("nom"),
-              rs.getString("email"));
-            
-            result.add(personne);
-        }
-        return result;
+
+  public static List<Evaluation> getEvaluationByFormateur(int idFormateur) throws SQLException {
+    Connection con = Database.getConnection();
+    List<Evaluation> result = new ArrayList();
+    String requete = "SELECT m.nom as nom_module, date_effet, s.nom as nom_session\n"
+            + "FROM evaluation e INNER JOIN module m\n"
+            + "ON e.id_module = m.id_module\n"
+            + "INNER JOIN session_formation s \n"
+            + "on s.id_session_formation =e.id_session_formation\n"
+            + "WHERE id_createur = ( SELECT id_personne \n"
+            + "                      FROM personne \n"
+            + "                      WHERE id_personne = ? and est_formateur=1)";
+    PreparedStatement eval = con.prepareStatement(requete);
+    eval.setInt(1, idFormateur);
+    ResultSet rs = eval.executeQuery();
+    while (rs.next()) {
+      Evaluation evaluation = new Evaluation(
+              rs.getTimestamp("date_effet").toLocalDateTime(),
+              rs.getString("nom_module"),
+              rs.getString("nom_session"));
+      result.add(evaluation);
     }
-  
+    return result;
+  }
+
 }
