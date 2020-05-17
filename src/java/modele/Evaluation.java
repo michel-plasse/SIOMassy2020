203c5ec1dddf6754057e7274bb3c9693bb55341a
reda Zeryouhi <reda.zeryouhi@gmail.com>
2020-05-18 01:59:21 +0200

    ajout deux attributs à Evaluation (String nomModule + String nomSession) et génération d'un nouveau constructeur + getters et setters

--- src/java/modele/Evaluation.java
+++ src/java/modele/Evaluation.java
@@ -4,63 +4,87 @@ import java.time.LocalDateTime;
 
 public class Evaluation {
 
-    private int idEvaluation;
-    private LocalDateTime dateEffet;
-    private int idCreateur;
-    private int idSessionFormation;
-    private int idModule;
+  private int idEvaluation;
+  private LocalDateTime dateEffet;
+  private int idCreateur;
+  private int idSessionFormation;
+  private int idModule;
+  private String nomModule;
+  private String nomSession;
 
-    // ##################################    Constructeurs    ############################################
-    public Evaluation() {
-    }
+  // ##################################    Constructeurs    ############################################
+  public Evaluation() {
+  }
 
-    public Evaluation(int idEvaluation, LocalDateTime dateEffet, int idCreateur, int idSessionFormation, int idModule) {
-        this.idEvaluation = idEvaluation;
-        this.dateEffet = dateEffet;
-        this.idCreateur = idCreateur;
-        this.idSessionFormation = idSessionFormation;
-        this.idModule = idModule;
-    }
+  public Evaluation(int idEvaluation, LocalDateTime dateEffet, int idCreateur, int idSessionFormation, int idModule) {
+    this.idEvaluation = idEvaluation;
+    this.dateEffet = dateEffet;
+    this.idCreateur = idCreateur;
+    this.idSessionFormation = idSessionFormation;
+    this.idModule = idModule;
+  }
 
-    // ############################### Getters et Setters ##################################################
-    public int getIdEvaluation() {
-        return idEvaluation;
-    }
+  public Evaluation(LocalDateTime dateEffet, String nomModule, String nomSession) {
+    this.dateEffet = dateEffet;
+    this.nomModule = nomModule;
+    this.nomSession = nomSession;
+  }
 
-    public void setIdEvaluation(int idEvaluation) {
-        this.idEvaluation = idEvaluation;
-    }
+  // ############################### Getters et Setters ##################################################
+  public int getIdEvaluation() {
+    return idEvaluation;
+  }
 
-    public LocalDateTime getDateEffet() {
-        return dateEffet;
-    }
+  public void setIdEvaluation(int idEvaluation) {
+    this.idEvaluation = idEvaluation;
+  }
 
-    public void setDateEffet(LocalDateTime dateEffet) {
-        this.dateEffet = dateEffet;
-    }
+  public LocalDateTime getDateEffet() {
+    return dateEffet;
+  }
 
-    public int getIdCreateur() {
-        return idCreateur;
-    }
+  public void setDateEffet(LocalDateTime dateEffet) {
+    this.dateEffet = dateEffet;
+  }
 
-    public void setIdCreateur(int idCreateur) {
-        this.idCreateur = idCreateur;
-    }
+  public int getIdCreateur() {
+    return idCreateur;
+  }
 
-    public int getIdSessionFormation() {
-        return idSessionFormation;
-    }
+  public void setIdCreateur(int idCreateur) {
+    this.idCreateur = idCreateur;
+  }
 
-    public void setIdSessionFormation(int idSessionFormation) {
-        this.idSessionFormation = idSessionFormation;
-    }
+  public int getIdSessionFormation() {
+    return idSessionFormation;
+  }
 
-    public int getIdModule() {
-        return idModule;
-    }
+  public void setIdSessionFormation(int idSessionFormation) {
+    this.idSessionFormation = idSessionFormation;
+  }
 
-    public void setIdModule(int idModule) {
-        this.idModule = idModule;
-    }
+  public int getIdModule() {
+    return idModule;
+  }
+
+  public void setIdModule(int idModule) {
+    this.idModule = idModule;
+  }
+
+  public String getNomModule() {
+    return nomModule;
+  }
+
+  public void setNomModule(String nomModule) {
+    this.nomModule = nomModule;
+  }
+
+  public String getNomSession() {
+    return nomSession;
+  }
+
+  public void setNomSession(String nomSession) {
+    this.nomSession = nomSession;
+  }
 
 }
