5c910f9da45721f46ec67a66b0f6e007c08340b3
MICHEL Baptiste <baptistemichel22@gmail.com>
2020-05-21 10:17:46 +0200

    rename resultatquestionnairedao dans modele par resultatquestionnaire

--- src/java/controller/ResultatQuestionnaireServlet.java
+++ src/java/controller/ResultatQuestionnaireServlet.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package controller;
+import dao.ResultatQuestionnaireDao;
+import modele.Personne;
+import modele.Questionnaire;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.annotation.WebServlet;
 
 /**
  *
  * @author wowmi
  */
-public class ResultatQuestionnaireServlet {
+@WebServlet("/questionnaires")
+public class ResultatQuestionnaireServlet extends HttpServlet {
     
+    private final String VUE_OK = "/WEB-INF/resultatquestionnaire.jsp";
+    private final String VUE_ERREUR = "/WEB-INF/exception.jsp";
 }
