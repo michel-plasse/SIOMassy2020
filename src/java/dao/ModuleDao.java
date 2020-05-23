b625067de1fb46d8822cdb07016fe86eca92c9f3
kenzy <kenzy@macbook-pro-de-kenzy.home>
2020-05-23 19:02:44 +0200

    modification de la classe ModuleDao.java

--- src/java/dao/ModuleDao.java
+++ src/java/dao/ModuleDao.java
@@ -9,12 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import modele.Module;
 
 /**
  *
