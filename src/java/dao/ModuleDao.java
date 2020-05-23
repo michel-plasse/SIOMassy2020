77938cb2c36947e44e7176df6374eda32b57ec79
kenzy <kenzy@kenzybnt@hotmail.com>
2020-05-23 19:01:35 +0200

    ajout de la classe ModuleDao.java

---
+++ src/java/dao/ModuleDao.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import modele.Module;
+
+/**
+ *
+ * @author kenzy
+ */
+public class ModuleDao {
+
+//  public static List<Module> getModule() throws SQLException {
+//    Connection con = Database.getConnection();
+//    List<Module> result = new ArrayList();
+//    String requete = "SELECT * FROM module ORDER BY nom ASC";
+//    PreparedStatement stmt = con.prepareStatement(requete);
+//    //canal.setInt(1, idFormateur);
+//    ResultSet rs = stmt.executeQuery();
+//    while (rs.next()) {
+//      Module m = new Module(
+//              rs.getString("nom"));
+//      result.add(m);
+//    }
+//    return result;
+//  }
+
+  public Map<Integer, String> getModule() throws SQLException {
+    Map<Integer, String> result = new HashMap<>();
+    Connection con = Database.getConnection();
+    String requete = "SELECT* FROM module ORDER BY nom ASC";
+    PreparedStatement stmt = con.prepareStatement(requete);
+    ResultSet rs = stmt.executeQuery();
+    while (rs.next()) {
+      result.put(rs.getInt("id_module"), rs.getString("nom"));
+    }
+    return result;
+  }
+
+}
